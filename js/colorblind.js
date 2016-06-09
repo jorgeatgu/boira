10844cb9e081c1e2ec6fa1c4b382fae670d7b9c1
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2016-06-09 22:21:46 +0200

    subiendo toda la estructura

---
+++ js/colorblind.js
@@ -0,0 +1,23 @@
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+// build items array
+var items = [{
+    src: 'https://placekitten.com/600/400',
+    w: 600,
+    h: 400
+}, {
+    src: 'https://placekitten.com/1200/900',
+    w: 1200,
+    h: 900
+}];
+
+// define options (if needed)
+var options = {
+    // optionName: 'option value'
+    // for example:
+    index: 0 // start at first slide
+};
+
+// Initializes and opens PhotoSwipe
+var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();
