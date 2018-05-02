b0c8bbdf89a2433cd588bccd0bad9fffb04e3db3
Jorge Aznar <jorgeatgu@gmail.com>
2018-05-02 20:16:58 +0200

    update Gulp, NPM and rules for stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -3,55 +3,89 @@ postcss = require('gulp-postcss');
 autoprefixer = require('gulp-autoprefixer');
 sourcemaps = require('gulp-sourcemaps');
 atImport = require('postcss-import');
-cssnext = require('postcss-cssnext');
+selector = require('postcss-custom-selectors')
+customProperties = require("postcss-custom-properties")
 sorting = require('postcss-sorting');
 nested = require('postcss-nested');
 pxtorem = require('postcss-pxtorem');
 reporter = require('postcss-reporter');
+imagemin = require('gulp-imagemin');
 uglify = require('gulp-uglify');
 newer = require('gulp-newer');
 nano = require('gulp-cssnano');
-notify = require("gulp-notify");
+notify = require('gulp-notify');
 stylelint = require('stylelint');
+browserSync = require('browser-sync');
+webp = require('gulp-webp');
+// responsive = require('gulp-responsive');
 
+var paths = {
+  js: 'src/js',
+  css: 'src/css',
+  images: 'src/img/*',
+  buildCss: 'css/',
+  buildJs: 'js/',
+  buildImages: 'img/'
+};
 
-/* Variables */
-var imgSrc = './src/img/*';
-var imgDist = './img';
-var jsSrc = './src/js/*.js';
-var jsDist = './js';
+var watch = {
+  js: [
+    paths.js + '/**/*.js'
+  ],
+  css: [
+    paths.css + '/**/*.css'
+  ],
+  minifycss: [
+    paths.buildCss + '/**/*.css'
+  ],
+  images: [
+    paths.images + '/**/*.*'
+  ],
+  html: [
+  '/*.html'
+  ]
+};
+
+gulp.task("browserSync", function() {
+    browserSync({
+        server: {
+            baseDir: "./",
+            reloadDelay: 200
+        }
+    })
+});
 
 /* Notificando errores de JavaScript */
 function errorAlertJS(error) {
-  notify.onError({
-    title: "Gulp JavaScript",
-    subtitle: "Algo esta mal en tu JavaScript!",
-    sound: "Basso"
-  })(error);
-  console.log(error.toString());
-  this.emit("end");
+    notify.onError({
+        title: "Gulp JavaScript",
+        subtitle: "Algo esta mal en tu JavaScript!",
+        sound: "Basso"
+    })(error);
+    console.log(error.toString());
+    this.emit("end");
 };
 
 /* Notificando errores de CSS */
 function errorAlertPost(error) {
-  notify.onError({
-    title: "Gulp postCSS",
-    subtitle: "Algo esta mal en tu CSS!",
-    sound: "Basso"
-  })(error);
-  console.log(error.toString());
-  this.emit("end");
+    notify.onError({
+        title: "Gulp postCSS",
+        subtitle: "Algo esta mal en tu CSS!",
+        sound: "Basso"
+    })(error);
+    console.log(error.toString());
+    this.emit("end");
 };
 
 /* Comprimiendo JavaScript */
 gulp.task('compress', function() {
-  return gulp.src(jsSrc)
-    .pipe(uglify())
-    .on("error", errorAlertJS)
-    .pipe(gulp.dest(jsDist))
-    .pipe(notify({
-      message: 'JavaScript complete'
-    }));
+    return gulp.src(watch.js)
+        .pipe(uglify())
+        .on("error", errorAlertJS)
+        .pipe(gulp.dest(paths.buildJs))
+        .pipe(notify({
+            message: 'JavaScript complete'
+        }));
 });
 
 /* ==========================================================================
@@ -63,76 +97,133 @@ gulp.task('compress', function() {
  *
  * Antes de que nuestro CSS empiece a ser transformado por los diferentes
  * plugins vamos a 'lintear' nuestro CSS para seguir un orden y concierto.
- * este párrafo.
+ *
  *
  */
 
 gulp.task('css', function() {
-  var processors = [
-    atImport,
-    nested,
-    stylelint(),
-    reporter({
-      clearMessages: true
-    }),
-    cssnext,
-    pxtorem({
-      root_value: 16,
-      unit_precision: 2,
-      prop_white_list: ['font', 'font-size', 'line-height', 'letter-spacing', 'margin', 'padding'],
-      replace: true,
-      media_query: false
-    }),
-    sorting({
-      "sort-order": "csscomb"
-    }),
-    autoprefixer
-  ];
-  return gulp.src('./src/css/styles.css')
+    var processors = [
+        atImport({
+            plugins: [stylelint]
+        }),
+        stylelint,
+        reporter({
+            clearMessages: true
+        }),
+        nested,
+        customProperties,
+        selector,
+        pxtorem({
+            root_value: 16,
+            unit_precision: 2,
+            prop_white_list: ['font', 'font-size', 'line-height', 'letter-spacing', 'margin', 'padding'],
+            replace: true,
+            media_query: false
+        }),
+        sorting({
+            "sort-order": "csscomb"
+        }),
+        autoprefixer
+    ];
+    return gulp.src('./src/css/styles.css')
+
     .pipe(sourcemaps.init())
-    .pipe(postcss(processors))
-    .on("error", errorAlertPost)
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'))
-    .pipe(notify({
-      message: 'postCSS complete'
-    }));
+        .pipe(postcss(processors))
+        .on("error", errorAlertPost)
+        .pipe(sourcemaps.write('./', {
+            sourceRoot: '/src'
+        }))
+        .pipe(gulp.dest(paths.buildCss))
+        .pipe(browserSync.stream())
+        .pipe(notify({
+            message: 'postCSS complete'
+        }));
 });
 
 /* Lanzando CSSnano para comprimir CSS */
 gulp.task('minify', function() {
-  return gulp.src('./css/styles.css')
-    .pipe(nano())
-    .pipe(gulp.dest('./css'))
-    .pipe(notify({
-      message: 'CSSnano task complete'
-    }));
+    return gulp.src(watch.minifycss)
+    //Remove comments false //Z index
+        .pipe(nano())
+        .pipe(gulp.dest(paths.buildCss))
+        .pipe(notify({
+            message: 'CSSnano task complete'
+        }));
 });
 
 /* Comprimiendo imagenes */
 gulp.task('imagemin', function() {
-  return gulp.src(imgSrc)
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{
-        removeViewBox: false
-      }],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest(imgDist));
+    return gulp.src(paths.images)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(paths.buildImages));
 });
 
 gulp.task('images', function() {
-  return gulp.src(imgSrc)
-    .pipe(newer(imgDist))
-    .pipe(imagemin())
-    .pipe(gulp.dest(imgDist));
+    return gulp.src(paths.images)
+        .pipe(newer(paths.images))
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.buildImages));
 });
+
+gulp.task('webp', () =>
+    gulp.src('img/*.jpg')
+        .pipe(webp())
+        .pipe(gulp.dest(paths.buildImages))
+);
+
+// gulp.task('imgrwd', function () {
+//   return gulp.src('src/img/*.{png,jpg}')
+//     .pipe(responsive({
+//       '*.png': [{
+//         width: 300,
+//         rename: {
+//           suffix: '-300px',
+//           extname: '.jpg',
+//         },
+//         format: 'jpeg',
+//       }, {
+//         width: 600,
+//         rename: {
+//           suffix: '-600px',
+//           extname: '.jpg',
+//         },
+//       }, {
+//         width: 1200,
+//         rename: {
+//           suffix: '-1200px',
+//           extname: '.jpg',
+//         }
+//       }],
+//     }, {
+//       quality: 75,
+//       progressive: true,
+//       withMetadata: false,
+//       errorOnEnlargement: false,
+//     }))
+//     .pipe(gulp.dest('img'));
+// });
 
 /* Tarea por defecto para compilar CSS y comprimir imagenes */
-gulp.task('default', function() {
-  gulp.watch('./src/css/*.css', ['css']);
+gulp.task('default', ["browserSync"], function() {
+    //Add interval to watcher!
+    gulp.watch(watch.css, { interval: 300 }, ['css']);
+    gulp.watch(watch.images, { interval: 300 }, ['images']);
+    gulp.watch(watch.js, { interval: 300 }, ['compress']);
+    gulp.watch(["./*.html", "css/*.css", "js/*.js"]).on("change", browserSync.reload);
 });
 
-/* Tarea final para comprimir CSS y JavaScript */
+/* Tarea final para comprimir CSS y JavaScript. Eliminar el CSS sin usar e incluirlo en línea en el HTML
+    Por último creamos las imágenes con diferentes tamaños y las pasamos a WebP.
+*/
+
+// Build para un proyecto sin imágenes
 gulp.task('build', ['minify', 'compress']);
+
+//Build para un proyecto con imágenes
+gulp.task('buildimg', ['minify', 'compress', 'imgrwd' , 'webp']);
